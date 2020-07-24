70eec508855019ff8df38066bc8f847395144977
Felipe99-CO <fg202069@gmail.com>
2020-07-23 23:26:45 -0500

    feat show tasks

--- js/main.js
+++ js/main.js
@@ -1,34 +1,49 @@
 // console.log(document.getElementById('formTask'));
 window.onload = function () {
     document.getElementById('formTask').addEventListener('submit', saveTask);
+    updateDashboardTask();
 
     function saveTask(e) {
         let title = document.getElementById('title').value;
         let description = document.getElementById('description').value;
-        // console.log(title);
-        // console.log(description);
-        const task = {
-            title,
-            description
-        };
-        if (localStorage.getItem("task") === null) {
-            let tasks = [];
-            tasks.push(task);
-            localStorage.setItem("task", JSON.stringify(tasks));
+        // Válidar formulario
+        if (title.trim() != '' && description.trim() != '') {
+            // Crear objeto
+            const task = {
+                title,
+                description
+            };
+            // Evaluar si existe en el localStorage
+            if (localStorage.getItem("task") === null) {
+                let tasks = [];
+                tasks.push(task);
+                localStorage.setItem("task", JSON.stringify(tasks));
+                updateDashboardTask();
+            } else {
+                let tasks = JSON.parse(localStorage.getItem('task'));
+                console.log(tasks);
+                tasks.push(task);
+                localStorage.setItem("task", JSON.stringify(tasks));
+                updateDashboardTask();
+            }
         } else {
-            let tasks = JSON.parse(localStorage.getItem('task'));
-            console.log(tasks);
-            tasks.push(task);
-            localStorage.setItem("task", JSON.stringify(tasks));
+            alert('Por favor, ingrese un valor válido.');
         }
-        // console.log(JSON.parse(localStorage.getItem('task')));
         e.preventDefault();
     }
 
     function updateDashboardTask() {
-        $tasks = JSON.parse(localStorage.getItem('task'));
-
-        
+        // Añadir al tablero
+        let tasks = JSON.parse(localStorage.getItem('task'));
+        let divTask = document.getElementById('tasks');
+        if (tasks) {
+            for (const task of tasks) {
+                let taskDesc = document.createElement('div');
+                taskDesc.classList.add('alert', 'alert-success');
+                taskDesc.textContent = `${task.title} | ${task.description}`;
+                divTask.appendChild(taskDesc);
+            }
+        }
     }
 }
 
