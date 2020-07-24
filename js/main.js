faf8c02b6f8c0a63e5c55edb619f7cd3a01137e9
Felipe99-CO <fg202069@gmail.com>
2020-07-24 00:03:30 -0500

    feat delete task

--- js/main.js
+++ js/main.js
@@ -32,18 +32,40 @@ window.onload = function () {
         e.preventDefault();
     }
 
-    function updateDashboardTask() {
-        // Añadir al tablero
-        let tasks = JSON.parse(localStorage.getItem('task'));
-        let divTask = document.getElementById('tasks');
-        if (tasks) {
-            for (const task of tasks) {
-                let taskDesc = document.createElement('div');
-                taskDesc.classList.add('alert', 'alert-success');
-                taskDesc.textContent = `${task.title} | ${task.description}`;
-                divTask.appendChild(taskDesc);
-            }
+}    
+
+function updateDashboardTask() {
+    // Añadir al tablero
+    let tasks = JSON.parse(localStorage.getItem('task'));
+    let divTask = document.getElementById('tasks');
+    divTask.innerHTML = '';
+
+    if (tasks) {
+        for (let [index, task] of tasks.entries()) {
+            // let taskDesc = document.createElement('div');
+            // taskDesc.classList.add('alert', 'alert-success');
+            // taskDesc.textContent = `${task.title} | ${task.description}`;
+            // divTask.appendChild(taskDesc);
+            divTask.innerHTML += `
+                <div class="card mt-2">
+                    <div class="card-header d-flex justify-content-between">
+                        <div class="text-header">${task.title}</div>
+                        <span class="btn btn-danger remove-task" data-id="${index}" onclick="deleteTask(this)"><i class="fa fa-times" aria-hidden="true"></i></span>
+                    </div>
+                    <div class="card-body">
+                        <div class="text-body">${task.description}</div>
+                    </div>
+                </div>`;
         }
     }
+}
+
+function deleteTask(e) {
+    // Obtener el id para eliminar elemento del array
+    let tasks = JSON.parse(localStorage.getItem('task'));
+    const $id = parseInt(e.getAttribute('data-id'));
+    tasks.splice($id, 1);
+    localStorage.setItem('task', JSON.stringify(tasks));
+    updateDashboardTask();
 }
 
