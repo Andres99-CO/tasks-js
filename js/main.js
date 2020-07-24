72951060ba30a764c8040331ccd12defc8a19a2f
Felipe99-CO <fg202069@gmail.com>
2020-07-23 22:58:51 -0500

    feat save localstorage

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
 // console.log(document.getElementById('formTask'));
-window.onload = function () { 
+window.onload = function () {
     document.getElementById('formTask').addEventListener('submit', saveTask);
-    
+
     function saveTask(e) {
         let title = document.getElementById('title').value;
         let description = document.getElementById('description').value;
         // console.log(title);
         // console.log(description);
-
+        const task = {
+            title,
+            description
+        };
+        if (localStorage.getItem("task") === null) {
+            let tasks = [];
+            tasks.push(task);
+            localStorage.setItem("task", JSON.stringify(tasks));
+        } else {
+            let tasks = JSON.parse(localStorage.getItem('task'));
+            console.log(tasks);
+            tasks.push(task);
+            localStorage.setItem("task", JSON.stringify(tasks));
+        }
+        // console.log(JSON.parse(localStorage.getItem('task')));
         e.preventDefault();
     }
- }
 
- 
+    function updateDashboardTask() {
+        $tasks = JSON.parse(localStorage.getItem('task'));
+
+        
+    }
+}
+
